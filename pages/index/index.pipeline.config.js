5eb0b22931be2ba9ede57501f9d0c7dab6716a63
shsgear <shsgear@qq.com>
2020-05-18 01:32:32 +0800

    add example

--- pages/index/index.pipeline.config.js
+++ pages/index/index.pipeline.config.js
@@ -5,7 +5,7 @@ export default {
     // 面向SEO渲染需要的 job 集合，一般要求是全部
     seoFetch: {
       type: 'parallel',
-      jobs: ['task1', 'task2']
+      jobs: ['task1', 'task2', 'task3', 'task4']
     },
     // 面向用户首屏渲染需要的最小的 job 集合
     minFetch: {
@@ -15,24 +15,50 @@ export default {
     // 首屏加载完之后，在 mounted 阶段异步执行的 job 集合
     mounted: {
       type: 'parallel',
-      jobs: []
+      jobs: ['task2', 'task3']
     },
     // 空闲时刻才执行的 job 集合
     idle: {
       type: 'serial',
-      jobs: []
+      jobs: ['task4']
     }
   },
   pipelines: {
     // 任务1
     task1: {
       task: ({ store, params, query, error, redirect, app, route }) => {
-        console.log('task111111111')
+        return new Promise(() => {
+          setTimeout(() => {
+            console.log('task1')
+          }, 1000)
+        })
       }
     },
     task2: {
       task: () => {
-        console.log('task2')
+        return new Promise(() => {
+          setTimeout(() => {
+            console.log('task2')
+          }, 1000)
+        })
+      }
+    },
+    task3: {
+      task: () => {
+        return new Promise(() => {
+          setTimeout(() => {
+            console.log('task3')
+          }, 1000)
+        })
+      }
+    },
+    task4: {
+      task: () => {
+        return new Promise(() => {
+          setTimeout(() => {
+            console.log('task4')
+          }, 1000)
+        })
       }
     }
   }
